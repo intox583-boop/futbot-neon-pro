034f5ef5dcad5fe5593916d53ea3564fe3ef673b
intox583-boop <intox583@gmail.com>
2026-06-29 19:36:13 -0300

    Versao 2.3.1 - PWA pronto para Store
    Versao 2.3.1 - PWA pronto para Store

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'futbot-neon-v231';
+const ARQUIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARQUIVOS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
